c7f9b1178ed3d72a49a4e748b7120803453ef118
Sahil <kazisahil2006@gmail.com>
2026-04-25 02:09:46 +0530

    added worst_fit.py

---
+++ worst_fit.py
@@ -0,0 +1,21 @@
+
+blocks = [200,100,323,67,78,232]
+psize = 212
+used = [False]*len(blocks)
+
+idx = -1
+mx = 0
+
+for i in range(len(blocks)):
+    if blocks[i] >= psize and not used[i]:
+        if blocks[i] > mx:
+            mx = blocks[i]
+            idx = i
+
+if idx == -1:
+    print("No block allocated")
+else:
+    used[idx] = True
+    print(f"Worst Fit: Block {blocks[idx]} allocated to {psize}")
+
+print(f"Occupancy: {used}")
